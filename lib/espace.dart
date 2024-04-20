daee92e215d52537dd2c80ee6deecfa17b4212dc
ranoucha10 <149247498+ranoucha10@users.noreply.github.com>
2024-04-20 15:35:10 +0100

    Update espace.dart

--- lib/espace.dart
+++ lib/espace.dart
@@ -1,6 +1,7 @@
-import 'package:autisme_app/login.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'loginenfant.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Page de Boutons',
+      title: 'Autismo',
       home: EspacePage(),
     );
   }
@@ -22,37 +23,74 @@ class EspacePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(''),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Image.asset(
-              'assets/choix.jpg',
-              fit: BoxFit.cover,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 0.7),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.all(20), // Réduit la valeur de 100 à 20
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30),
+                        child: Image.asset(
+                          'assets/logo.jpg',
+                          width: 150,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                        print('Parent Space Button Pressed');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(300, 60),
+                      ),
+                      child: Text('Espace de Parent'),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginenfantPage()),
+                        );
+                        print('Child Space Button Pressed');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(200, 50),
+                        minimumSize:
+                            Size(double.infinity, 50), // Correction ici
+                      ),
+                      child: Text('Espace de Enfant'),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-              print('Parent Space Button Pressed');
-            },
-            child: Text('Espace de Parent'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-               Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-              print('Child Space Button Pressed');
-            },
-            child: Text('Espace de Enfant'),
-          ),
-        ],
+        ),
       ),
     );
   }
