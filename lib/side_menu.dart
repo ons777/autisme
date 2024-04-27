0d87087c9784df1e5c677c5104866edf1a1bb9ea
ranoucha10 <149247498+ranoucha10@users.noreply.github.com>
2024-04-27 13:57:46 +0100

    Update side_menu.dart

--- lib/side_menu.dart
+++ lib/side_menu.dart
@@ -43,7 +43,8 @@ class _SideMenuPageState extends State<SideMenuPage> {
     super.initState();
     fetchUserInfo();
   }
-   // tsawer par défaut 
+
+  // tsawer par défaut
   List<String> profilePictures = [
     'gnome1.jpg',
     'gnome2.jpg',
@@ -54,64 +55,101 @@ class _SideMenuPageState extends State<SideMenuPage> {
     'gnome7.jpg',
     'gnome8.jpg',
   ];
-  // tsawer par défaut 
+  // tsawer par défaut
   String getRandomProfilePicture() {
     Random random = Random();
     int index = random.nextInt(profilePictures.length);
     return profilePictures[index];
   }
+
   // Function to handle user account creation
   Future<void> createUserAccount() async {
-  try {
-    // Get the current user
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      // Check if the user already has a profile picture assigned
-      final DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
+    try {
+      // Get the current user
+      final User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        // Check if the user already has a profile picture assigned
+        final DocumentSnapshot<Map<String, dynamic>> snapshot =
+            await FirebaseFirestore.instance
+                .collection('users')
+                .doc(user.uid)
+                .get();
 
-      if (!snapshot.exists || snapshot.data()?['profilePictureUrl'] == null) {
-        // Generate a random profile picture URL only if it's not already set
-        String profilePictureUrl = getRandomProfilePicture();
-        // Update the user's profile picture URL in the database
-        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
-          'profilePictureUrl': profilePictureUrl,
-        }, SetOptions(merge: true));
+        if (!snapshot.exists || snapshot.data()?['profilePictureUrl'] == null) {
+          // Generate a random profile picture URL only if it's not already set
+          String profilePictureUrl = getRandomProfilePicture();
+          // Update the user's profile picture URL in the database
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .set({
+            'profilePictureUrl': profilePictureUrl,
+          }, SetOptions(merge: true));
+        }
       }
+    } catch (e) {
+      print('Error creating user account: $e');
     }
-  } catch (e) {
-    print('Error creating user account: $e');
   }
-}
 
-
-   Future<void> fetchUserInfo() async {
+  Future<void> fetchUserInfo() async {
     try {
       final User? user = FirebaseAuth.instance.currentUser;
       if (user != null) {
-        final QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
-            .collection('users')
-            .where('email', isEqualTo: user.email)
-            .get();
+        final QuerySnapshot<Map<String, dynamic>> querySnapshot =
+            await FirebaseFirestore.instance
+                .collection('users')
+                .where('email', isEqualTo: user.email)
+                .get();
 
         if (querySnapshot.docs.isNotEmpty) {
           final userData = querySnapshot.docs.first.data();
           setState(() {
+            // Mettre à jour le nom de l'utilisateur
             userName = userData['name'];
+
+            // Mettre à jour l'e-mail de l'utilisateur
             userEmail = user.email!;
           });
+        } else {
+          // S'il n'y a pas de document correspondant à l'e-mail de l'utilisateur
+          print('No user data found for email: ${user.email}');
         }
+      } else {
+        // Si l'utilisateur est null
+        print('Current user is null');
       }
     } catch (e) {
+      // En cas d'erreur lors de la récupération des informations de l'utilisateur
       print('Error fetching user info: $e');
     }
   }
 
   Future<String> fetchUserProfilePicture() async {
-    // Code pour récupérer l'image du profil de l'utilisateur
-    return 'path_vers_l_image'; // Remplacez par le chemin réel de l'image
+    try {
+      final User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        final DocumentSnapshot<Map<String, dynamic>> snapshot =
+            await FirebaseFirestore.instance
+                .collection('users')
+                .doc(user.uid)
+                .get();
+
+        if (snapshot.exists) {
+          // Check if the user has a profile picture set in the database
+          final String? profilePicture = snapshot.data()?['profilePicture'];
+          if (profilePicture != null && profilePicture.isNotEmpty) {
+            return profilePicture; // Return user profile picture URL
+          }
+        }
+      }
+    } catch (e) {
+      print('Error fetching user profile picture: $e');
+    }
+
+    // If user profile picture is not found, return a random one from the list
+    String randomPicture = getRandomProfilePicture();
+    return 'assets/$randomPicture';
   }
 
   @override
@@ -126,7 +164,7 @@ class _SideMenuPageState extends State<SideMenuPage> {
               style: const TextStyle(color: Colors.black),
             ),
             accountEmail: Text(
-              userEmail.isNotEmpty ? userName : '',
+              userEmail.isNotEmpty ? userEmail : '',
               style: const TextStyle(color: Colors.black),
             ),
             currentAccountPicture: FutureBuilder<String>(
