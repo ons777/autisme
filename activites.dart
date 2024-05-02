644f43a4b4a18bd464f6ed707bc21f87cccb6083
ons777 <161731281+ons777@users.noreply.github.com>
2024-05-02 21:46:42 +0100

    Update activites.dart

--- activites.dart
+++ activites.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: library_private_types_in_public_api, duplicate_ignore
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -78,13 +79,10 @@ class Category {
   }
 }
 
-
-
 class ActivityPage extends StatefulWidget {
   const ActivityPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _ActivityPageState createState() => _ActivityPageState();
 }
 
@@ -99,20 +97,24 @@ class _ActivityPageState extends State<ActivityPage> {
   }
 
   void resetActivities() {
-  setState(() {
-    for (var category in categories) {
-      for (var activity in category.activities) {
-        activity.isDone = false;  // Reset to false
-        activity.isMarked = null;
+    setState(() {
+      for (var category in categories) {
+        for (var activity in category.activities) {
+          activity.isDone = false;  // Reset to false
+          activity.isMarked = null;
+        }
       }
-    }
-  });
-}
+    });
+  }
 
+  // Function to add an activity
+  void addNewActivity(String name, String time, String  image) {
+    setState(() {
+      categories[selectedCategoryIndex].activities.add(Activity(name: name, time: time, image: image),);
+    });
+  }
 
-
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -122,7 +124,7 @@ class _ActivityPageState extends State<ActivityPage> {
             icon: const Icon(Icons.refresh),
             onPressed: resetActivities,
           ),
-          ],
+        ],
       ),
 
       body: Column(
@@ -162,15 +164,202 @@ class _ActivityPageState extends State<ActivityPage> {
                 childAspectRatio: 1 / 1.2,
               ),
               itemBuilder: (context, index) {
-                return ActivityTile(activity: categories[selectedCategoryIndex].activities[index]);
+                return ActivityTile(
+                  activity: categories[selectedCategoryIndex].activities[index],
+                );
               },
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+  onPressed: () {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AddActivityDialog(
+          onActivityAdded: addNewActivity,
+        );
+      },
+    );
+  },
+  child: const Icon(Icons.add),
+),
+
     );
   }
 }
+
+class AddActivityDialog extends StatefulWidget {
+  final Function(String name, String time, String image) onActivityAdded;
+
+  const AddActivityDialog({super.key, required this.onActivityAdded});
+
+  @override
+  _AddActivityDialogState createState() => _AddActivityDialogState();
+}
+
+class _AddActivityDialogState extends State<AddActivityDialog> {
+  final TextEditingController _nameController = TextEditingController();
+  String _time = '';
+  String _imagePath = '';
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _time = picked.format(context);
+      });
+    }
+  }
+
+  Future<void> _getImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _imagePath = image.path;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: contentBox(context),
+    );
+  }
+
+  contentBox(context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 20),
+          margin: const EdgeInsets.only(top: 20),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(0, 10),
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text(
+                'Add Activity',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () => _selectTime(context),
+                    icon: const Icon(Icons.access_time),
+                    label: const Text('Select Time'),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    onPressed: _getImage,
+                    icon: const Icon(Icons.image),
+                    label: const Text('Select Image'),
+                  ),
+                ],
+              ),
+              if (_imagePath.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Image.file(
+                    File(_imagePath),
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  OutlinedButton(
+  onPressed: () {
+    Navigator.of(context).pop();
+  },
+  style: OutlinedButton.styleFrom(
+    foregroundColor: const Color.fromARGB(255, 99, 43, 254), side: const BorderSide(color: Color.fromARGB(255, 99, 43, 254)), // Border color
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+  ),
+  child: const Text(
+    'Cancel',
+    style: TextStyle(fontSize: 18),
+  ),
+),
+
+
+
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      final name = _nameController.text.trim();
+                      if (name.isNotEmpty && _time.isNotEmpty && _imagePath.isNotEmpty) {
+                        widget.onActivityAdded(name, _time, _imagePath);
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: const Text(
+                      'Add Activity',
+                      style: TextStyle(fontSize: 18),
+                    ),
+
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 20,
+          right: 20,
+          child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 20,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              child: Container(
+                color: Colors.blue,
+                child: const Icon(Icons.add, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 
 
 class CategoryPage extends StatelessWidget {
@@ -211,14 +400,14 @@ class ActivityTile extends StatefulWidget {
 }
 
 class _ActivityTileState extends State<ActivityTile> {
-  bool _showIcons = false;  // Add this line
+  bool _showIcons = false;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(  // Replace GestureDetector with InkWell
+    return InkWell(
       onTap: () {
         setState(() {
-          _showIcons = !_showIcons;  // Toggle the visibility of the icons
+          _showIcons = !_showIcons;
         });
       },
       child: Card(
