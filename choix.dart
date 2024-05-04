4b6ffc043759d35b44a928ab3933358556ed0022
ranoucha10 <149247498+ranoucha10@users.noreply.github.com>
2024-05-04 13:15:55 +0100

    Update choix.dart

--- choix.dart
+++ choix.dart
@@ -1,33 +1,31 @@
-import 'package:autisme_app/activites.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'ChatPage .dart';
+import 'activites.dart';
 import 'formulaire.dart';
 import 'profile.dart';
 import 'ressources.dart';
 import 'side_menu.dart';
+import 'thememodel.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Autisme App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Choix(
-        userEmail: '',
-        userName: '',
-        informations: {},
+void main() => runApp(
+      ChangeNotifierProvider<ThemeModel>(
+        create: (_) => ThemeModel(),
+        child: MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            fontFamily: 'Montserrat',
+          ),
+          home: const Choix(
+            userEmail: '',
+            userName: '',
+            informations: {},
+          ),
+        ),
       ),
     );
-  }
-}
 
 class Choix extends StatefulWidget {
   final Map<String, String> informations;
@@ -35,7 +33,7 @@ class Choix extends StatefulWidget {
   final String userName;
 
   const Choix({
-    super.key,
+    Key? key,
     required this.informations,
     required this.userEmail,
     required this.userName,
@@ -49,11 +47,13 @@ class Enfant {
   final String emailenfant;
   final String motDePasse;
   final String pseudo;
+  final String emailParent;
 
   Enfant({
     required this.emailenfant,
     required this.motDePasse,
     required this.pseudo,
+    required this.emailParent,
   });
 }
 
@@ -67,136 +67,173 @@ class _ChoixState extends State<Choix> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Liste des enfants'),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu_rounded),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
-          ),
-        ),
-      ),
-      drawer: const Drawer(child: SideMenuPage()),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _addEnfant,
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: const Color.fromARGB(255, 80, 142, 209),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40.0),
+    return Consumer<ThemeModel>(
+      builder: (context, themeProvider, child) {
+        return Scaffold(
+          backgroundColor:
+              themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
+          appBar: AppBar(
+            title: Text('Liste des enfants',
+                style: TextStyle(
+                    color: themeProvider.isDarkMode
+                        ? Colors.white
+                        : Colors.black)),
+            elevation: 0,
+            backgroundColor:
+                themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
+            leading: Builder(
+              builder: (context) => IconButton(
+                icon: Icon(Icons.menu_rounded,
+                    color:
+                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
               ),
-              padding: const EdgeInsets.all(20.0),
             ),
-            child: const Text('Ajouter Enfant'),
           ),
-          const SizedBox(height: 20),
-          // create a stream builer to load all enfant under the user connected
-          Expanded(
-              child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('enfants')
-                .where('emailParent', isEqualTo: getEmail)
-                .snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return Center(
-                  child: Text('Aucun enfant trouvé.'),
-                );
-              }
+          drawer: const Drawer(child: SideMenuPage()),
+          body: Column(
+            children: [
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _addEnfant(getEmail!),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color.fromARGB(255, 80, 142, 209),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  padding: const EdgeInsets.all(20.0),
+                ),
+                child: const Text('Ajouter Enfant'),
+              ),
+              const SizedBox(height: 20),
+              // create a stream builer to load all enfant under the user connected
+              Expanded(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('enfants')
+                      .where('emailParent', isEqualTo: getEmail)
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return Center(
+                        child: Text('Aucun enfant trouvé.'),
+                      );
+                    }
 
-              // display enfants in a list
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  DocumentSnapshot document = snapshot.data!.docs[index];
-                  Map<String, dynamic> data =
-                      document.data() as Map<String, dynamic>;
+                    // display enfants in a list
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot document = snapshot.data!.docs[index];
+                        Map<String, dynamic> data =
+                            document.data() as Map<String, dynamic>;
 
-                  return Card(
-                    child: ListTile(
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Email enfant:',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ), // Mettre en gras
-                          ),
-                          Text('${data['emailenfant']}'),
-                          SizedBox(height: 8), // Espacement
-
-                          Text(
-                            'Pseudo:',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ), // Mettre en gras
-                          ),
-                          Text('${data['pseudo']}'),
-                        ],
-                      ),
-                      // set destination widget when clicking on enfant
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const ActivityPage(),
+                        return Card(
+                          color: themeProvider.isDarkMode
+                              ? Colors.grey[800]
+                              : Colors
+                                  .white, // Définir la couleur de fond en fonction du thème sombre ou clair
+                          child: ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${data['pseudo']}',
+                                  style: TextStyle(
+                                      color: themeProvider.isDarkMode
+                                          ? Colors.white
+                                          : Colors.black),
+                                ),
+                              ],
+                            ),
+                            // set destination widget when clicking on
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ActivityPage(),
+                                ),
+                              );
+                            },
                           ),
                         );
                       },
-                    ),
-                  );
-                },
-              );
-            },
-          )),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
-          BottomNavigationBarItem(icon: Icon(Icons.folder), label: 'Resources'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
-      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor:
+                themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
+            type: BottomNavigationBarType.shifting,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat,
+                    color:
+                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                label: 'Chat',
+                backgroundColor: themeProvider.isDarkMode
+                    ? Colors.grey[900]
+                    : Colors.grey[200],
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.folder,
+                    color:
+                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                label: 'Resources',
+                backgroundColor: themeProvider.isDarkMode
+                    ? Colors.grey[900]
+                    : Colors.grey[200],
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person,
+                    color:
+                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                label: 'Profile',
+                backgroundColor: themeProvider.isDarkMode
+                    ? Colors.grey[900]
+                    : Colors.grey[200],
+              ),
+            ],
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            onTap: _onItemTapped,
+          ),
+        );
+      },
     );
   }
 
-  Future<void> _addEnfant() async {
+  Future<void> _addEnfant(String emailParent) async {
     final emailenfantMotDePasse = await showDialog<Map<String, String>>(
       context: context,
-      builder: (context) => FormulaireDialog(userEmail: 'email@example.com'),
+      builder: (context) => FormulaireDialog(userEmail: emailParent),
     );
     if (emailenfantMotDePasse != null) {
       final nouvelEnfant = Enfant(
         emailenfant: emailenfantMotDePasse['emailenfant']!,
         motDePasse: emailenfantMotDePasse['motDePasse']!,
         pseudo: emailenfantMotDePasse['pseudo']!,
+        emailParent: emailParent,
       );
       setState(() => enfants.add(nouvelEnfant));
       await ajouterEnfant(
         nouvelEnfant.pseudo,
         nouvelEnfant.emailenfant,
         nouvelEnfant.motDePasse,
-        'email@example.com',
+        nouvelEnfant.emailParent,
       );
     }
   }
@@ -220,47 +257,17 @@ class _ChoixState extends State<Choix> {
   void _onItemTapped(int index) {
     switch (index) {
       case 0:
-        if (ModalRoute.of(context)!.settings.name != '/choix') {
-          Navigator.pushNamed(context, '/choix');
-        }
-        break;
-      case 1:
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => const ChatPage()));
         break;
-      case 2:
+      case 1:
         Navigator.push(context,
             MaterialPageRoute(builder: (context) => const RessourcesPage()));
         break;
-      case 3:
+      case 2:
         Navigator.push(context,
             MaterialPageRoute(builder: (context) => const ProfilePage()));
         break;
     }
-  }
-
-  void _showEnfantDetails(BuildContext context, Enfant enfant) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(enfant.pseudo),
-          content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Email: ${enfant.emailenfant}'),
-              // Add more details as needed
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
   }
 }
