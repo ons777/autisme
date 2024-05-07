84b6c032ca4208ada6777ee77d4d4fcfd942ba3b
ranoucha10 <149247498+ranoucha10@users.noreply.github.com>
2024-05-07 14:06:33 +0100

    Update espace.dart

--- espace.dart
+++ espace.dart
@@ -1,19 +1,15 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'loginenfant.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Autismo',
       home: EspacePage(),
     );
@@ -21,74 +17,75 @@ class MyApp extends StatelessWidget {
 }
 
 class EspacePage extends StatelessWidget {
-  const EspacePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(255, 255, 255, 0.7),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.jpg'),
+              fit: BoxFit.cover,
             ),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding:
-                    const EdgeInsets.all(20), // Réduit la valeur de 100 à 20
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 30),
-                        child: Image.asset(
-                          'assets/logo.jpg',
-                          width: 150,
+          ),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 0.7),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.all(20), // Réduit la valeur de 100 à 20
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 30),
+                          child: Image.asset(
+                            'assets/logo.jpg',
+                            width: 150,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 60),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const LoginPage()),
-                        );
-                        print('Parent Space Button Pressed');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(300, 60),
+                      SizedBox(height: 60),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()),
+                          );
+                          print('Parent Space Button Pressed');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(300, 60),
+                        ),
+                        child: Text('Espace  Parents'),
                       ),
-                      child: const Text('Espace  Parents'),
-                    ),
-                    const SizedBox(height: 60),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginenfantPage()),
-                        );
-                        print('Child Space Button Pressed');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(300, 60),
-                        // Correction ici
-                      ),
-                      child: const Text('Espace  Enfants'),
-                    )
-                  ],
+                      SizedBox(height: 60),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginenfantPage()),
+                          );
+                          print('Child Space Button Pressed');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(300, 60),
+                          // Correction ici
+                        ),
+                        child: Text('Espace  Enfants'),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
