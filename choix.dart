c18b884bb9f9ebd4f66ba90f1c3f47cad4661a68
ons777 <161731281+ons777@users.noreply.github.com>
2024-05-08 16:56:58 +0100

    Update choix.dart

--- choix.dart
+++ choix.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names, library_private_types_in_public_api, avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'ChatPage .dart';
+import 'ChatPage.dart';
 import 'activites.dart';
 import 'formulaire.dart';
 import 'profile.dart';
@@ -33,7 +35,7 @@ class Choix extends StatefulWidget {
   final String userName;
 
   const Choix({
-    Key? key,
+    super.key,
     required this.informations,
     required this.userEmail,
     required this.userName,
@@ -73,19 +75,23 @@ class _ChoixState extends State<Choix> {
           backgroundColor:
               themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
           appBar: AppBar(
-            title: Text('Liste des enfants',
-                style: TextStyle(
-                    color: themeProvider.isDarkMode
-                        ? Colors.white
-                        : Colors.black)),
+            title: Text(
+              'Liste des enfants',
+              style: TextStyle(
+                color:
+                    themeProvider.isDarkMode ? Colors.white : Colors.black,
+              ),
+            ),
             elevation: 0,
             backgroundColor:
                 themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
             leading: Builder(
               builder: (context) => IconButton(
-                icon: Icon(Icons.menu_rounded,
-                    color:
-                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                icon: Icon(
+                  Icons.menu_rounded,
+                  color:
+                      themeProvider.isDarkMode ? Colors.white : Colors.black,
+                ),
                 onPressed: () {
                   Scaffold.of(context).openDrawer();
                 },
@@ -93,114 +99,167 @@ class _ChoixState extends State<Choix> {
             ),
           ),
           drawer: const Drawer(child: SideMenuPage()),
-          body: Column(
+          body: Stack(
             children: [
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _addEnfant(getEmail!),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color.fromARGB(255, 80, 142, 209),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40.0),
+              // Background Image
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/Dream_clouds.jpeg'),
+                    fit: BoxFit.cover,
                   ),
-                  padding: const EdgeInsets.all(20.0),
                 ),
-                child: const Text('Ajouter Enfant'),
               ),
-              const SizedBox(height: 20),
-              // create a stream builer to load all enfant under the user connected
-              Expanded(
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('enfants')
-                      .where('emailParent', isEqualTo: getEmail)
-                      .snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return Center(
-                        child: Text('Aucun enfant trouvé.'),
-                      );
-                    }
-
-                    // display enfants in a list
-                    return ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        DocumentSnapshot document = snapshot.data!.docs[index];
-                        Map<String, dynamic> data =
-                            document.data() as Map<String, dynamic>;
-
-                        return Card(
-                          color: themeProvider.isDarkMode
-                              ? Colors.grey[800]
-                              : Colors
-                                  .white, // Définir la couleur de fond en fonction du thème sombre ou clair
-                          child: ListTile(
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '${data['pseudo']}',
-                                  style: TextStyle(
-                                      color: themeProvider.isDarkMode
-                                          ? Colors.white
-                                          : Colors.black),
-                                ),
-                              ],
+              Column(
+                children: [
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () => _addEnfant(getEmail!),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color.fromARGB(255, 80, 142, 209),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 20.0),
+                    ),
+                    icon: const Icon(Icons.add, size: 24),
+                    label: const Text(
+                      'Ajouter Enfant',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Create a stream builder to load all enfant under the user connected
+                  Expanded(
+                    child: StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('enfants')
+                          .where('emailParent', isEqualTo: getEmail)
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (!snapshot.hasData ||
+                            snapshot.data!.docs.isEmpty) {
+                          return const Center(
+                            child: Text(
+                              'Aucun enfant trouvé.',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
                             ),
-                            // set destination widget when clicking on
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ActivityPage(),
+                          );
+                        }
+
+                        // Display enfants in a list
+                        return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot document =
+                                snapshot.data!.docs[index];
+                            Map<String, dynamic> data =
+                                document.data() as Map<String, dynamic>;
+
+                            return Card(
+                              color: themeProvider.isDarkMode
+                                  ? Colors.grey[800]
+                                  : Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              elevation: 8,
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 16),
+                              child: ListTile(
+                                leading: const CircleAvatar(
+                                  backgroundColor: Colors.blueAccent,
+                                  child: Icon(Icons.child_care,
+                                      color: Colors.white),
                                 ),
-                              );
-                            },
-                          ),
+                                title: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${data['pseudo']}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: themeProvider.isDarkMode
+                                            ? Colors.white
+                                            : Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                // Set destination widget when clicking on
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ActivityPage(),
+                                    ),
+                                  );
+                                },
+                              ),
+                            );
+                          },
                         );
                       },
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.endDocked,
           bottomNavigationBar: BottomNavigationBar(
-            backgroundColor:
-                themeProvider.isDarkMode ? Colors.grey[900] : Colors.grey[200],
+            backgroundColor: themeProvider.isDarkMode
+                ? Colors.grey[900]
+                : Colors.grey[200],
             type: BottomNavigationBarType.shifting,
             items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.chat,
-                    color:
-                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                icon: Icon(
+                  Icons.chat,
+                  color: themeProvider.isDarkMode
+                      ? Colors.white
+                      : Colors.black,
+                ),
                 label: 'Chat',
                 backgroundColor: themeProvider.isDarkMode
                     ? Colors.grey[900]
                     : Colors.grey[200],
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.folder,
-                    color:
-                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                icon: Icon(
+                  Icons.folder,
+                  color: themeProvider.isDarkMode
+                      ? Colors.white
+                      : Colors.black,
+                ),
                 label: 'Resources',
                 backgroundColor: themeProvider.isDarkMode
                     ? Colors.grey[900]
                     : Colors.grey[200],
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.person,
-                    color:
-                        themeProvider.isDarkMode ? Colors.white : Colors.black),
+                icon: Icon(
+                  Icons.person,
+                  color: themeProvider.isDarkMode
+                      ? Colors.white
+                      : Colors.black,
+                ),
                 label: 'Profile',
                 backgroundColor: themeProvider.isDarkMode
                     ? Colors.grey[900]
